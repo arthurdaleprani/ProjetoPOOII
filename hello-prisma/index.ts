da8fc0839320b0e6842b0b6dd0c9d98b4d19bf73
Arthur Daleprani <102961025+arthurdaleprani@users.noreply.github.com>
2023-08-31 20:32:29 -0300

    Add files via upload

--- hello-prisma/index.ts
+++ hello-prisma/index.ts
@@ -14,11 +14,39 @@ async function main() {
           telefone: '27999123453',
         },
       });
-  
+       
+    const newMae = await prisma.mae.create({
+        data: {
+          idMae:1,
+          endereco: 'Rua Martins, 66',
+          nome: 'Julia Lopes',
+          telefone: '2799967586',
+          dataNasc:'1990-05-20',
+        },
+      });
+    
+      const newBebe = await prisma.bebe.create({
+        data: {
+          idBebe:1,
+          idMed: 1,
+          idMae: 1,
+          nome: 'Lucas Lopes',
+          dataNasc: '2023-08-30',
+          pesoNasc: '3.2 kg',
+          nomeMae: 'Julia Lopes',
+          altura: '50 cm',
+        },
+      });
+    
+    
+
+
+
    
       await prisma.$disconnect();
     }
 
+    
     
 
 
